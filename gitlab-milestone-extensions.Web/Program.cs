46a2a48adaeee656458ee680ede965c904347300
kiyokawa <kiyokawa.masaki@nisshinbo.co.jp>
2026-07-08 09:05:03 +0900

    update

--- gitlab-milestone-extensions.Web/Program.cs
+++ gitlab-milestone-extensions.Web/Program.cs
@@ -1,7 +1,9 @@
 using gitlab_milestone_extensions.Web;
 using gitlab_milestone_extensions.Web.Services;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
